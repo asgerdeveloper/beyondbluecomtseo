c3e52fa30627ae8fce3a2c1b5bd73bb8e8677beb
asgerdeveloper <127490290+asgerdeveloper@users.noreply.github.com>
2023-07-31 10:54:44 +0600

    Update index.en.js

--- src/pages/couples-therapy/index.en.js
+++ src/pages/couples-therapy/index.en.js
@@ -24,7 +24,7 @@ const Index = props => {
                     method trained therapist
                   </h2>
                   <p className="text-lg">
-                  <ul>
+                  <ul className="list-disc">
                     <li>Do you want to reduce conflict and improve communication?</li>
                     <li>Do you long for a way to rebuild trust and connection?</li>
                     <li>Are you looking for greater emotional or sexual intimacy?</li>
@@ -54,7 +54,7 @@ const Index = props => {
                     couples achieve a long-term healthy relationship.
                     <strong>Some skills that you and your spouse or partner will learn
                       include:</strong>
-                      <ul>
+                      <ul className="list-disc">
                         <li>Work through and resolve conflict, even around arguments
                           that you’ve been having for years</li>
                           <li>Develop a greater sense of understanding and appreciation for
