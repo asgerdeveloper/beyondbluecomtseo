0c3be36f371cdc71ff33412bd6daadb2122753b2
asgerdeveloper <127490290+asgerdeveloper@users.noreply.github.com>
2023-07-31 12:14:00 +0600

    Update index.en.js

--- src/pages/couples-therapy/index.en.js
+++ src/pages/couples-therapy/index.en.js
@@ -90,7 +90,7 @@ const Index = props => {
               </p>
             </div>
                           <div class="aspect-w-16 aspect-h-9">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/Ap1TxCYZCHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <iframe width="auto" height="auto" src="https://www.youtube.com/embed/Ap1TxCYZCHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>
 
           <div className="text-center md:text-left">
